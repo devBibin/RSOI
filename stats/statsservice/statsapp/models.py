6ead9e35bd0e255afe86d17d252326628adfe69f
Владимир Бибин <devbibinva@gmail.com>
2017-11-26 18:25:02 +0300

    Stats service added

--- stats/statsservice/statsapp/models.py
+++ stats/statsservice/statsapp/models.py
@@ -3,4 +3,18 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class TestAnswer(models.Model):
+	test_id = models.IntegerField()
+	question_id = models.IntegerField()
+	choice_id = models.IntegerField()
+	user_id = models.IntegerField()
+	is_true = models.BooleanField()
+
+class CreativeTaskAnswer(models.Model):
+	user_id = models.IntegerField()
+	task_id = models.IntegerField()
+	status = models.CharField(max_length=20, default ="processing")
+	rate = models.IntegerField()
+	remark = models.CharField(max_length =1500)
+
+		
--- stats/statsservice/statsservice/settings.py
+++ stats/statsservice/statsservice/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'statsapp.apps.StatsappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -72,11 +73,14 @@ WSGI_APPLICATION = 'statsservice.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'db_courses_test',
+        'USER': 'vladimir',
+        'PASSWORD': '000',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
