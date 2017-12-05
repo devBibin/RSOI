2b1383bf47b0f8a20b60d1e3c38d9e3361ed4a7f
Владимир Бибин <devbibinva@gmail.com>
2017-12-05 14:32:47 +0300

    test script for all services added

---
+++ test_script.sh
@@ -0,0 +1,25 @@
+cd gateway/gatewayservice/
+python manage.py test
+cd ../../
+
+cd creative_tasks/creativeservice/
+python manage.py test
+cd ../../
+
+cd billing/billingservice/
+python manage.py test
+cd ../../
+
+
+cd stats/statsservice/
+python manage.py test
+cd ../../
+
+
+cd testing/testingservice/
+python manage.py test
+cd ../../
+
+cd user/userservice/
+python manage.py test
+cd ../../
